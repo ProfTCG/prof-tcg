8f5a116ed12ee5c64e40e539f5de40a27d0cb77a
Lucas Horsman <lucashorsman@gmail.com>
2024-04-11 23:57:23 -1000

    finish marketplace mockup
    I also moved card.js to the proper location --will fix in the merge

---
+++ app/imports/ui/pages/Marketplace.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+// import { Cards } from '../../api/stuff/Cards';
+
+const Marketplace = () => (
+  <Container>
+    <Row>
+      <Col>
+        <h1>Marketplace</h1>
+        <p>Here you can see all cards available for trade from other users!
+          WIP!
+        </p>
+      </Col>
+      <Col>
+        <h2>Card List</h2>
+        <p>These cards are for sale!</p>
+        <ul>
+          <img src="/images/johnson-card-mockup.png" alt="Philip Johnson" width={200} />
+          {/* {Cards.collection.find().map((card) => <li key={card._id}>{card.profname}</li>)} */}
+        </ul>
+        <ul>Make Trade Request Button</ul>
+      </Col>
+    </Row>
+
+  </Container>
+);
+
+export default Marketplace;

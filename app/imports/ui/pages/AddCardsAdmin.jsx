0839bcedbd5035d8e02e059ba0c0b404418dec08
Samantha Mallari <samiam2204@gmail.com>
2024-04-23 12:02:45 -1000

    Added labels to form fields

--- app/imports/ui/pages/AddCardsAdmin.jsx
+++ app/imports/ui/pages/AddCardsAdmin.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import { AutoForm, ErrorsField, NumField, BoolField, SubmitField, TextField } from 'uniforms-bootstrap5';
 import swal from 'sweetalert';
-import { Meteor } from 'meteor/meteor';
+// import { Meteor } from 'meteor/meteor';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
-import { Stuffs } from '../../api/stuff/Stuff';
+import { Cards } from '../../api/stuff/Cards.js';
 
 // Create a schema to specify the structure of the data to appear in the form.
 const formSchema = new SimpleSchema({
@@ -26,7 +26,7 @@ const AddCardsAdmin = () => {
   // On submit, insert the data.
   const submit = (data, formRef) => {
     const { profName, rarity, profImage, border, backImage, backText, isForSale } = data;
-    Stuffs.collection.insert(
+    Cards.collection.insert(
       { profName, rarity, profImage, border, backImage, backText, isForSale },
       (error) => {
         if (error) {
@@ -45,20 +45,20 @@ const AddCardsAdmin = () => {
     <Container className="py-3">
       <Row className="justify-content-center">
         <Col xs={5}>
-          <Col className="text-center"><h2>Add Stuff</h2></Col>
+          <Col className="text-center"><h2>Add Card</h2></Col>
           <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
             <Card>
               <Card.Body>
-                <TextField extra="Professor Name" name="profName" />
+                <TextField label="Professor Name" name="profName" />
                 {
                   // may need to set max to 5
                 }
                 <NumField min="1" max="4" name="rarity" decimal={null} />
-                <TextField name="profImage" />
-                <TextField name="border" />
-                <TextField name="backImage" />
-                <TextField name="backText" />
-                <BoolField name="isForSale" />
+                <TextField label="Professor Image" name="profImage" />
+                <TextField label="Border" name="border" />
+                <TextField label="Back Image" name="backImage" />
+                <TextField label="Back Text" name="backText" />
+                <BoolField label="Is for Sale?" name="isForSale" />
                 <SubmitField value="Submit" />
                 <ErrorsField />
               </Card.Body>

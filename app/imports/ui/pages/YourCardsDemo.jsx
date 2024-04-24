27aa63d41ca6d18ea767c5be3d127b1b680817b0
Lucas Horsman <lucashorsman@gmail.com>
2024-04-23 18:56:20 -1000

    first attempt at tests

---
+++ app/imports/ui/pages/YourCardsDemo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import CardCloseups from '../components/CardCloseups';
+
+const YourCardsDemo = () => (
+  <Container id="your-cards">
+    <div>
+      <CardCloseups />
+    </div>
+  </Container>
+);
+
+export default YourCardsDemo;
--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -1,7 +1,11 @@
 import { landingPage } from './landing.page';
 import { signinPage } from './signin.page';
 import { signoutPage } from './signout.page';
+import { yourcardsPage } from './yourcards.page';
+import { addcardsPage } from './addcards.page';
 import { navBar } from './navbar.component';
+import { marketplacePage } from './marketplace.page';
+import { encyclopediaPage } from './encyclopedia.page';
 
 /* global fixture:false, test:false */
 
@@ -13,6 +17,18 @@ fixture('meteor-application-template-react localhost test with default db')
 
 test('Test that landing page shows up', async (testController) => {
   await landingPage.isDisplayed(testController);
+});
+test('Test that "your cards" page shows up', async (testController) => {
+  await yourcardsPage.isDisplayed(testController);
+});
+test('Test that addcards page shows up', async (testController) => {
+  await addcardsPage.isDisplayed(testController);
+});
+test('Test that marketplace page shows up', async (testController) => {
+  await marketplacePage.isDisplayed(testController);
+});
+test('Test that encyclopedia page shows up', async (testController) => {
+  await encyclopediaPage.isDisplayed(testController);
 });
 
 test('Test that signin and signout work', async (testController) => {
